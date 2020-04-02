41d167993e33e9c3df5e3b6fe771d09377901f1d
raulgp19 <55979635+raulgp19@users.noreply.github.com>
2020-04-02 17:00:28 +0200

    Actividad1_Maven
    Actividad para jitpack

---
+++ Actividad1_Maven/src/main/java/Copyright.java
@@ -0,0 +1,13 @@
+public class Copyright {
+
+    public String getText()
+    {
+        return "Copyright (C) 2020 Raúl García Pacheco - All Rights Reserved\n" +
+                "        You may use, distribute and modify this code under the\n" +
+                "        terms of the PACHEKO license, which unfortunately won't be\n" +
+                "        written for another century.\n" +
+                "        \n" +
+                "        You should have received a copy of the PACHEKO license with\n" +
+                "        this file. If not, please visit : http://www.github.com";
+    }
+}
